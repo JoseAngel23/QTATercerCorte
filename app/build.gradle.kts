bd2aaa78306d97d5deeee9c6eaed49410536e4e0
Jose Manuel Forero Angel <josem.foreroa@utadeo.edu.co>
2024-10-24 19:06:55 -0500

    Agregando configuraciones para fragmentos

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -36,13 +36,14 @@ android {
 }
 
 dependencies {
-
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
+    implementation("androidx.fragment:fragment-ktx:1.8.4")
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
 }
+
